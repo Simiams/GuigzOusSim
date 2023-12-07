1e145f1933b3dd05ba6584b6b83d7dca2b8a4d62
Simiams <simon.convert@epsi.fr>
2023-12-07 18:18:16 +0100

    add some view

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+
 def see_all_pokedexes(request):
-    return render(request, 'pokedex/see_all_pokedexes.html')
+    return render(request, 'pokedex/pages/index.html')
+def see_pokedex_by_id(request, id):
+    return render(request, 'pokedex/pages/pokedex.html')
