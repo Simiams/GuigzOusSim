2bd8fb7f3da3dbf4fc81572ca4d0e6e97b0cbe2d
Simiams <simon.convert@epsi.fr>
2023-12-08 11:49:37 +0100

    putain

--- teams/views.py
+++ teams/views.py
@@ -4,8 +4,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .api import get_pokemon_by_id
-from .models import Pokemon, PokemonTeam
+from .models import PokemonTeam
 from .serializers import PokemonSerializer
+
 
 # Create your views here.
 
@@ -26,6 +27,13 @@ def see_homepage(request):
         for p in t.members.all():
             print(p.id)
 
-    pokemons_by_team = [{"team": t, "pokemons": get_pokemon_by_id(t.members.all())} for t in pokemon_teams]
-    pokemons_by_team_api = [ for p in pokemons_by_team]
+    pokemons_by_team = [{"team": t, "pokemons": convert_pokemon_in_bdd_in_pokemon_in_api(t.members.all())} for t in
+                        pokemon_teams]
+
     return render(request, 'homepage.html', {"pokemon_teams": "pokemons_by_team"})
+
+
+def convert_pokemon_in_bdd_in_pokemon_in_api(pokemon_in_bdd):
+    pokemon_in_api = get_pokemon_by_id(pokemon_in_bdd.id)
+    pokemon_in_api["team"] = pokemon_in_bdd.team
+    return pokemon_in_api
