ac3b74f544904c8bc9cda4322635215dc7af1ff9
Simiams <simon.convert@epsi.fr>
2023-12-08 13:25:57 +0100

    format: [{"team": numero de la team, "pokemons": [PokemonDTO]}]

---
+++ teams/dtos.py
@@ -0,0 +1,53 @@
+class PokemonDTO:
+    # def __init__(self, name, sprite_front, sprite_back, type, weight, height, stats, abilities):
+    #     self.name = name
+    #     self.sprite_front = sprite_front
+    #     self.sprite_back = sprite_back
+    #     self.type = type
+    #     self.weight = weight
+    #     self.height = height
+    #     self.stats = stats
+    #     self.abilities = abilities
+
+    def __str__(self):
+        return f"{self.name} - {self.type} - {self.weight} - {self.height} - {self.stats} - {self.abilities}"
+
+    def __init__(self, p):
+        self.name = p["name"]
+        self.sprite_front = p["sprites"]["front_default"]
+        self.sprite_back = p["sprites"]["back_default"]
+        self.type = p["types"]
+        self.weight = p["weight"]
+        self.height = p["height"]
+        self.stats = [StatDTO(s) for s in p['stats']]
+        self.abilities = [AbilityDTO(a) for a in p['abilities']]
+
+
+class StatDTO:
+    # def __init__(self, effort, base_stat, name, url):
+    #     self.effort = effort
+    #     self.base_stat = base_stat
+    #     self.name = name
+    #     self.url = url
+
+    def __str__(self):
+        return f"{self.name} - {self.effort} - {self.base_stat}"
+
+    def __init__(self, s):
+        self.effort = s["effort"]
+        self.base_stat = s["base_stat"]
+        self.name = s["stat"]["name"]
+        self.url = s["stat"]["url"]
+
+
+class AbilityDTO:
+    # def __init__(self, name, url):
+    #     self.name = name
+    #     self.url = url
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __init__(self, a):
+        self.name = a["ability"]["name"]
+        self.url = a["ability"]["url"]
--- teams/views.py
+++ teams/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .api import get_pokemon_by_id
+from .dtos import PokemonDTO
 from .models import PokemonTeam
 from .serializers import PokemonSerializer
 
@@ -23,17 +24,18 @@ def create_pokemon(request):
 
 def see_homepage(request):
     pokemon_teams = PokemonTeam.objects.all()
-    for t in pokemon_teams:
-        for p in t.members.all():
-            print(p.id)
 
-    pokemons_by_team = [{"team": t, "pokemons": convert_pokemon_in_bdd_in_pokemon_in_api(t.members.all())} for t in
+    pokemons_by_team = [{"team": t, "pokemons": convert_pokemon_bdd_in_pokemon_dto(t.members.all())} for t in
                         pokemon_teams]
+    print(pokemons_by_team)
+    return render(request, 'homepage.html', {"pokemon_teams": pokemons_by_team})
 
-    return render(request, 'homepage.html', {"pokemon_teams": "pokemons_by_team"})
+
+def convert_pokemon_bdd_in_pokemon_dto(pokemons_in_bdd):
+    pokemons_in_api = []
+    for pokemon_in_bdd in pokemons_in_bdd:
+        pokemon_in_api = get_pokemon_by_id(pokemon_in_bdd.poke_api_id)
+        pokemons_in_api.append(pokemon_in_api)
+    return [PokemonDTO(p) for p in pokemons_in_api]
 
 
-def convert_pokemon_in_bdd_in_pokemon_in_api(pokemon_in_bdd):
-    pokemon_in_api = get_pokemon_by_id(pokemon_in_bdd.id)
-    pokemon_in_api["team"] = pokemon_in_bdd.team
-    return pokemon_in_api
